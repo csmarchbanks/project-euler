957470d07c7f24d8a1c1e516c1fe438e5f18141a
Chris Marchbanks <csmarchbanks@gmail.com>
2015-08-21 20:10:21 -0700

    Added solution to project 7

--- src/main/java/Problem6.java
+++ src/main/java/Problem6.java
@@ -9,12 +9,15 @@ public class Problem6 {
     }
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         long sum = LongStream.rangeClosed(1,100)
                 .sum();
 
         long sumOfSquares = LongStream.rangeClosed(1, 100)
                 .map(Problem6::square)
                 .sum();
+        long endTime = System.currentTimeMillis();
         System.out.println(sum*sum - sumOfSquares);
+        System.out.println("Calculation completed in: " + (endTime - startTime) + " ms");
     }
 }
---
+++ src/main/java/Problem7.java
@@ -0,0 +1,33 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by cmarchbanks on 8/21/15.
+ */
+public class Problem7 {
+    private static Set<Long> primes = new LinkedHashSet<>();
+
+    private static boolean isPrime(long n){
+        if(n == 1) return false;
+        if(primes.contains(n)) return true;
+
+        if(n == 2){
+            primes.add(2L);
+            return true;
+        } else {
+            boolean isPrime = !primes.stream().anyMatch(i -> n%i == 0);
+            if(isPrime){
+                primes.add(n);
+            }
+            return isPrime;
+        }
+    }
+
+    public static void main(String[] args) {
+        long n = 2;
+        while(primes.size() < 10_001){
+            isPrime(n++);
+        }
+        System.out.println(primes.stream().max(Long::compareTo).get());
+    }
+}
